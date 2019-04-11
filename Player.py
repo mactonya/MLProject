002b0d5432e2eabaceb08de58f7ad4613a3bc37e
Tsai Smith <smith1234567890@msn.com>
2019-04-11 13:14:03 +0800

    Add something

--- Player.py
+++ Player.py
@@ -5,3 +5,24 @@ class Player(object):
 		self.color = color		# Pos	
 		self.points = 0			# Points
 		self.is_banned = False	# If allowed to do a move
+		self.is_followed = False # If followed
+
+class Deck():
+	def __init__ (self, cards = []):
+		self.cards = cards
+	def rmv(self, cards):
+		self.cards -= cards
+	def add(self, cards):
+		if isinstance(cards, Deck):
+			self.cards += cards
+		else:
+			self.cards.append(cards)
+
+class Card():
+	def __init__ (self, color, ID, name):
+		self.color = color
+		self.ID = ID
+		self.name = name
+
+global_deck = Deck()
+global_deck.add(Card('B', 1, "Water"))
